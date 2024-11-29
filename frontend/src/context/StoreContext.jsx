6d7ea98119f9e87b03ce4ca65e607d50d2257c5b
vinay pratap <125810756+vinaypratapsinghrathore@users.noreply.github.com>
2024-11-29 23:25:24 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-   const url="https://food-del-backend-ttxr.onrender.com"
+   const url="http://localhost:5000"
    const [token,setToken]=  useState("");
    const [food_list,setFoodList] = useState([])
   const addToCart = async (itemId) => {
